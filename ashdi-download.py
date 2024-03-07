f75f809bb74301177f61dac9414aeb3ef805d2b9
Mystic-Mirage <mm@m10e.net>
2024-03-07 02:58:12 +0200

    Add more typing

--- ashdi-download.py
+++ ashdi-download.py
@@ -16,35 +16,37 @@ from ffmpeg.asyncio import FFmpeg
 @optgroup.option("-s", "--season", metavar="URL", multiple=True)
 @click.option("-q", "--quality", type=int)
 @click.option("-o", "--output-format", default="mp4")
-async def cli(episode, season, quality, output_format):
+async def cli(
+    episode: list[str] | None,
+    season: list[str] | None,
+    quality: int,
+    output_format: str,
+):
     if episode:
         await download_multiple(download_episode, episode, quality, output_format)
     elif season:
         await download_multiple(download_season, season, quality, output_format)
 
 
-def aiohttp_session(coro):
-    @wraps(coro)
+def aiohttp_session(f):
+    @wraps(f)
     async def wrapper(*args, session=None, **kwargs):
         if session:
-            return await coro(*args, session=session, **kwargs)
+            return await f(*args, session=session, **kwargs)
 
         async with ClientSession() as session:
-            return await coro(*args, session=session, **kwargs)
+            return await f(*args, session=session, **kwargs)
 
     return wrapper
 
 
 @aiohttp_session
-async def download_multiple(
-    coro, urls, quality, output_format, *, session: ClientSession
-):
-    await asyncio.gather(
-        *[coro(url, quality, output_format, session=session) for url in urls]
-    )
+async def download_multiple(f, urls, quality, output_format, *, session: ClientSession):
+    coros = [f(url, quality, output_format, session=session) for url in urls]
+    await asyncio.gather(*coros)
 
 
-async def get_player_url(url, *, session: ClientSession):
+async def get_player_url(url: str, *, session: ClientSession):
     async with session.get(url) as response:
         text = await response.text()
 
@@ -53,7 +55,7 @@ async def get_player_url(url, *, session: ClientSession):
         return tag["src"]
 
 
-async def get_quality_url(url, *, session: ClientSession):
+async def get_quality_url(url: str, *, session: ClientSession):
     async with session.get(url) as response:
         text = await response.text()
 
@@ -63,7 +65,7 @@ async def get_quality_url(url, *, session: ClientSession):
     return re.search(pattern, tag.text).group(1)
 
 
-async def get_episode_url(url, quality, *, session: ClientSession):
+async def get_episode_url(url: str, quality: int, *, session: ClientSession):
     async with session.get(url) as response:
         text = await response.text()
 
@@ -76,14 +78,16 @@ async def get_episode_url(url, quality, *, session: ClientSession):
     return url
 
 
-async def download_playlist(url, output_format):
+async def download_playlist(url: str, output_format: str):
     _, name, _, _, _ = url.rsplit("/", 4)
     output = f"{name}.{output_format}"
     ffmpeg = FFmpeg().option("y").input(url).output(output, c="copy")
     await ffmpeg.execute()
 
 
-async def download_episode(url, quality, output_format, *, session: ClientSession):
+async def download_episode(
+    url: str, quality: int, output_format: str, *, session: ClientSession
+):
     with suppress(ClientError):
         if player_url := await get_player_url(url, session=session):
             quality_url = await get_quality_url(player_url, session=session)
@@ -91,20 +95,19 @@ async def download_episode(url, quality, output_format, *, session: ClientSessio
             await download_playlist(episode_url, output_format)
 
 
-async def get_sub_urls(url, *, session: ClientSession):
+async def get_episode_urls(url: str, *, session: ClientSession):
     async with session.get(url) as response:
         text = await response.text()
 
     bs = BeautifulSoup(text, "html.parser")
-    urls = [
-        tag["href"]
-        for tag in bs.find_all("a", href=lambda value: value.startswith(url))
-    ]
+    urls = [tag["href"] for tag in bs.find_all("a", href=lambda v: v.startswith(url))]
     return urls
 
 
-async def download_season(url, quality, output_format, *, session: ClientSession):
-    urls = await get_sub_urls(url, session=session)
+async def download_season(
+    url: str, quality: int, output_format: str, *, session: ClientSession
+):
+    urls = await get_episode_urls(url, session=session)
     await download_multiple(
         download_episode, urls, quality, output_format, session=session
     )
